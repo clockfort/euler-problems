c74a8412372502ea5f21550d2e8660ec96f065c1
Chris Lockfort <clockfort@redhat.com>
2012-10-10 02:22:47 -0400

    problem 35

---
+++ problem35/problem35.hs
@@ -0,0 +1,13 @@
+import Data.Numbers.Primes
+import Data.List
+import Data.List.Ordered
+
+rotate xs n = take (length xs) $ drop (length xs + n) $ cycle xs
+
+rotations list = [ rotate list n | n <- [1.. (length list)]]
+
+strRot n = [read s :: Integer | s <- (rotations $ show n)]
+isCirclePrime n = (Data.List.Ordered.nub (sort (strRot n))) `subset` possibilities
+
+possibilities = take 78497 primes
+main = putStrLn  $ show $ length [ n | n <- possibilities, isCirclePrime n]
