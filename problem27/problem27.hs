bc7f36f1d4945c2f2941edae9c1ac70f615368cc
Chris Lockfort <clockfort@csh.rit.edu>
2013-06-11 00:03:36 -0400

    problem 27

---
+++ problem27/problem27.hs
@@ -0,0 +1,11 @@
+import Data.List
+import Data.List.Ordered
+import Data.Numbers.Primes
+import Data.Function
+
+testCoefficients a b = [n^2 + a*n + b | n <-[0..]]
+
+numPrimes a b = length $ takeWhile (has primes) (testCoefficients a b)
+
+main = putStrLn $ show $ maximumBy (compare `on` fst) [ (numPrimes a b, a*b) | a<-[-1000..1000], b<-[-1000..1000]]
+
