eb1f4e3d89c9146afc7c94ad2cf819710ad5c49d
Chris Lockfort <clockfort@csh.rit.edu>
2013-06-12 01:21:11 -0400

    better bounds

--- problem32/problem32.hs
+++ problem32/problem32.hs
@@ -2,4 +2,4 @@ import Data.List
 
 pandigitalProduct a b = sort (show a ++ show b ++ show (a*b)) == concatMap show [1..9]
 
-main = print $ show $ (sum . nub) [ a*b | a<-[1..2000], b<-[1..2000], pandigitalProduct a b]
+main = print $ show $ (sum . nub) [ a*b | a<-[1..100], b<-[1..2000], pandigitalProduct a b]
