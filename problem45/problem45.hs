0b138c6e5db08a7793f0b274c87d8850bff8d26a
Chris Lockfort <clockfort@csh.rit.edu>
2013-07-15 02:04:28 -0700

    Create problem45.hs

---
+++ problem45/problem45.hs
@@ -0,0 +1,8 @@
+import Data.List.Ordered
+import Data.Numbers.Primes
+
+main = print $ head $ filter (\n -> (n `member`  hexagonals) && ( n `member` pentagonals)) triangles
+
+triangles   = map (floor . (\n -> n*(  n+1)/2)) [286..]
+pentagonals = map (floor . (\n -> n*(3*n-1)/2)) [1..]
+hexagonals  = map          (\n -> n*(2*n-1))    [1..]
